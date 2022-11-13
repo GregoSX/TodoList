df2249f256a888c78cf850a926e12ac715081257
GregoSX <guilhermegrego376@gmail.com>
2022-11-13 11:00:18 -0300

    feat: transição de rotas

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -13,10 +13,20 @@
   
       </b-collapse>
     </b-navbar>
-    <router-view/>
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <style>
+  .fade-enter-active, .fade-leave-active {
+    transition-duration: .2s;
+    transition-property: opacity;
+    transition-timing-function: ease;
+  }
 
+  .fade-enter, .fade-leave {
+    opacity: 0;
+  }
 </style>
